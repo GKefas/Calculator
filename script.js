b634b7d88a0cdd7028fdf39e15c93346ea976900
George Kefas <kefasgeo04@gmail.com>
2024-03-08 18:41:39 +0200

    finished!

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-const display=document.getElementById('display');
-const previousDisplay=document.getElementById('PreviousActionsDisplay');
+const display = document.getElementById('display');
+const previousDisplay = document.getElementById('PreviousActionsDisplay');
+const clear = document.getElementById('clear');
 
-
-const calc = (htmlDIV) =>{
-
-    if (htmlDIV=='AC'){
-        display.innerText=' ';
-        previousDisplay.innerText=' ';
+const appendToDisplay = (input) =>{
+    if (display.textContent==='ERROR'){
+        display.textContent='';
     }
-
-    if (htmlDIV=='7'){
-        display.innerText+=7;
-        previousDisplay.innerText+='7';
-    }
-    
-
+    display.textContent +=input;
 }
 
+const clearDisplay = () =>{
+    previousDisplay.textContent='';
+    display.textContent="";
+}
+
+const remove = () =>{
+    if (display.textContent !== '' && display.textContent !== 'ERROR'){
+        display.textContent=display.textContent.slice(0,-1);
+        if (display.textContent.charAt(display.textContent.length-1) === '.'){
+            display.textContent=display.textContent.slice(0,-1);
+        }
+    }
+}
+
+const calculate = () =>{
+    try {
+        previousDisplay.textContent=display.textContent;
+        result = eval(display.textContent);
+        if (result === Infinity || result === -Infinity){
+            display.textContent='';
+            previousDisplay.textContent='';
+            const error = document.createElement('span');
+            error.classList.add("error");
+            error.textContent="ERROR";
+            display.appendChild(error);
+            return;
+        }
+        display.textContent=result;
+
+        
+    }catch(errors){
+        display.textContent='';
+        previousDisplay.textContent='';
+        const error = document.createElement('span');
+        error.classList.add("error");
+        error.textContent="ERROR";
+        display.appendChild(error);
+    }
+    
+}
